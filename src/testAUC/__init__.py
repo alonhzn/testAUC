e827a7c7682869f795cc7d6a8f36d76efcba1df7
alonhzn <1833092+alonhzn@users.noreply.github.com>
2023-12-16 16:45:48 -0500

    fix import

--- src/testAUC/__init__.py
+++ src/testAUC/__init__.py
@@ -1,3 +1,3 @@
-from src.testAUC.evaluate_auc import roc_drift, val_tst_colored_roc_curve, colored_roc_curve,dashboard
-from src.testAUC.evaluate_auc import noise_robustness, bias_robustness, plot_noise_robustness, plot_bias_robustness
-from src.testAUC.evaluate_auc import plot_wasserstein_distance_matrix, plot_predictions_hist
+from testAUC.evaluate_auc import roc_drift, val_tst_colored_roc_curve, colored_roc_curve,dashboard
+from testAUC.evaluate_auc import noise_robustness, bias_robustness, plot_noise_robustness, plot_bias_robustness
+from testAUC.evaluate_auc import plot_wasserstein_distance_matrix, plot_predictions_hist
